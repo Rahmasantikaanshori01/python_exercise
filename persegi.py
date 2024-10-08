318ac412ad669d539b1b0840152c3406dbe4fc81
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-10-08 08:58:26 +0700

    create aplikasi persegi

---
+++ persegi.py
@@ -0,0 +1,10 @@
+print('=' *40)
+print ('RUMUS PEESEGI')
+print('=' *40)
+
+sisi = int(input('sisi: '))
+luas = sisi * sisi
+keliling = 4 * sisi
+
+print('luas= ',luas)
+print('keliling= ',keliling)
