648195d25cb3d4ba1d6b5b2ef2b3338638649ee2
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-11-07 14:59:10 +0700

    create aplikasi persegi panjang

---
+++ persegi panjang.py
@@ -0,0 +1,16 @@
+# Dibuat oleh : Rahma Santika Al Anshori
+# Tanggal pengerjaan : 7 November 2024
+# Program persegi panjang
+
+print('-' * 40)
+print("RUMUS PERSEGI PANJANG")
+print('-' * 40)
+
+panjang = int(input('Panjang: '))
+lebar = int(input('Lebar: '))
+
+luas = panjang * lebar
+keliling = 2 * (panjang + lebar)
+
+print('Luas =', luas, "m2")
+print('Keliling =', keliling, "m")
