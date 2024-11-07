a89b77f62abd0495c4669bfd601627c7ede76e28
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-11-07 14:40:13 +0700

    create aplikasi belah ketupat

---
+++ belah ketupat.py
@@ -0,0 +1,17 @@
+# Dibuat oleh : Rahma Santika Al Anshori
+# Tanggal pengerjaan : 7 November 2024
+# Program belah ketupat
+
+print('-' * 40)
+print("RUMUS BELAH KETUPAT")
+print('-' * 40)
+
+diagonal1 = int(input('Diagonal 1: '))
+diagonal2 = int(input('Diagonal 2: '))
+sisi = int(input('Sisi: '))
+
+luas = (diagonal1 * diagonal2) / 2
+keliling = 4 * sisi
+
+print('Luas =', luas, "m2")
+print('Keliling =',keliling,"m")
