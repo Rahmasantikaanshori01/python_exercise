a75bf13c02f0f55bfdc22658cb61b005053ecde8
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-11-07 14:08:23 +0700

    create aplikasi segi lima

---
+++ segi lima.py
@@ -0,0 +1,14 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 7 November 2024
+# program Segi lima
+
+print('=' *40)
+print ('RUMUS SEGI LIMA')
+print('=' *40)
+
+sisi = int(input('sisi: '))
+luas = (1/4) * (5 * (5 + 2 * (5))) * sisi * sisi
+keliling = 5 * sisi
+
+print('Luas =', luas, "m2")
+print('Keliling =',keliling,"m")
