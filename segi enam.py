ce756b7c89b722a4691c1562528f9b9fbcdf936f
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-11-07 13:53:55 +0700

    create aplikasi segi enam

---
+++ segi enam.py
@@ -0,0 +1,14 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 7 November 2024
+# program Segi Enam
+
+print('=' *40)
+print ('RUMUS SEGI ENAM')
+print('=' *40)
+
+sisi = int(input('sisi: '))
+luas = (3 * (3) / 2) * sisi * sisi
+keliling = 6 * sisi
+
+print('Luas =', luas, "m2")
+print('Keliling =', keliling, "m")
