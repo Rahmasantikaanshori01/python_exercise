cd3fc2bf23e69d693cd07707ee8b99fd37f5484f
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-11-07 13:30:17 +0700

    sreat aplikasi kubus

---
+++ kubus.py
@@ -0,0 +1,14 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 7 November 2024
+# program kubus
+
+print('=' *40)
+print ('RUMUS KUBUS')
+print('=' *40)
+
+sisi = int(input('sisi: '))
+luas_permukaan = 6 * sisi * sisi
+volume = sisi * sisi * sisi
+
+print('Luas Permukaan =', luas_permukaan, "m2")
+print('Volume =', volume, "m3")
