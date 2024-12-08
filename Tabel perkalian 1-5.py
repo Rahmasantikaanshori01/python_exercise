b0b23f74e2be1131c25c333f1beb5cbd9340834e
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-12-08 15:33:28 +0700

    create program tabel perkalian 1-5

---
+++ Tabel perkalian 1-5.py
@@ -0,0 +1,31 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 08 Desember 2024
+# program Tabel Perkalian 1-5
+
+num = 1
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+print('\n')
+
+num = 2
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+print('\n')
+
+num = 3
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+print('\n')
+
+num = 4
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
+print('\n')
+
+num = 5
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
