44e335caf379d2f0150847ee163eca3c69268242
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2024-12-08 15:11:39 +0700

    create program looping2

---
+++ Looping2.py
@@ -0,0 +1,48 @@
+for nilai in range(5):
+    # print(nilai)
+    print(nilai+1,'. HELLO WORD')
+
+print('\n')
+
+for w in range(10):
+    # print(nilai)
+    print(w+2015,'. since')
+
+print('\n')
+
+for w in range(10):
+    # print(nilai)
+    print('since ',2024-w)
+
+print('\n')
+
+for w in range(1,10):
+    # print(nilai)
+    print(w+1,'. HELLO WORD')
+
+print('\n')
+
+for w in range(1,10,2):
+    # print(nilai)
+    print(w,'. HELLO WORD')
+
+print('\n')
+
+for w in range(1,10,3):
+    # print(nilai)
+    print(w+1,'. HELLO WORD')
+
+print('\n')
+
+for w in range(5):
+    print('CERDAS ',end=' ')
+
+print('\n')
+
+for w in "ABCD":
+    print(w,'. CERDAS ')
+
+print('\n')
+
+for w in "ABCD":
+    print(w,'. CERDAS ',end='\t')
