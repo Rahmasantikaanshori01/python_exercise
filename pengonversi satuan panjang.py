01f8337e4b00bd2607bae14b7749e828bf1583df
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2025-01-11 23:22:52 +0700

    create program konversi satuan panjang

---
+++ pengonversi satuan panjang.py
@@ -0,0 +1,12 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 11 januari 2025
+# program pengonversi satuan panjang
+
+print('-'*40)
+print("\tPENGONVERSI SATUAN PANJANG")
+print('-'*40)
+
+meter = float(input("Masukkan panjang (meter): "))
+print(f"Kilometer: {meter / 1000}")
+print(f"Centimeter: {meter * 100}")
+print(f"Mil: {meter/1609.34}")
