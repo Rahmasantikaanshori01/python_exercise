904f1530dfa64032a77e43c4725754756f7e0053
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2025-01-11 22:31:02 +0700

    create program wolume kerucut

---
+++ volume kubus.py
@@ -0,0 +1,12 @@
+# Dibuat oleh : Rahma Santika Al anshori
+# Tanggal pengerjaan : 10 januari 2025
+# program volume kerucut
+
+print('-'*40)
+print("\t VOLUME KERUCUR")
+print('-'*40)
+
+def volume_kerucut(jari_jari, tinggi):
+    return (1/3) * (jari_jari ** 2) * tinggi
+
+print("volume kerucut:", volume_kerucut(7, 10), "cm^3")
